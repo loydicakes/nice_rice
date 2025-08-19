ea458baf162898207fa04b111ed56a26a629319c
JDgayagoy <jdgprofessional@gmail.com>
2025-08-19 19:31:50 +0800

    Gradle Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,6 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-
-// Optional: keep for your existing HomePage button if desired
-Future<void> signInAnon() async {
-  final cred = await FirebaseAuth.instance.signInAnonymously();
-  debugPrint('Signed in: ${cred.user?.uid}');
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -74,85 +67,3 @@ class AuthGate extends StatelessWidget {
     );
   }
 }
-
-class AppShell extends StatefulWidget {
-  const AppShell({super.key});
-
-  @override
-  State<AppShell> createState() => _AppShellState();
-}
-
-class _AppShellState extends State<AppShell> {
-  final _controller = PageController();
-  int _index = 0;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _onTapTab(int i) {
-    setState(() => _index = i);
-    _controller.animateToPage(
-      i,
-      duration: const Duration(milliseconds: 280),
-      curve: Curves.easeOutCubic,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _controller,
-        physics: const BouncingScrollPhysics(),
-        onPageChanged: (i) => setState(() => _index = i),
-        children: const [HomePage(), AutomationPage(), AnalyticsPage()],
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(24),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(24),
-                border: Border.all(color: Colors.white.withOpacity(0.2)),
-              ),
-              child: NavigationBar(
-                height: 65,
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                indicatorColor: Colors.transparent,
-                selectedIndex: _index,
-                onDestinationSelected: _onTapTab,
-                destinations: const [
-                  NavigationDestination(
-                    icon: Icon(Icons.home_outlined),
-                    selectedIcon: Icon(Icons.home, color: Color.fromARGB(255, 45, 79, 43)),
-                    label: 'Home',
-                  ),
-                  NavigationDestination(
-                    icon: Icon(Icons.auto_awesome_motion_outlined),
-                    selectedIcon: Icon(Icons.auto_awesome_motion, color: Colors.white),
-                    label: 'Automation',
-                  ),
-                  NavigationDestination(
-                    icon: Icon(Icons.analytics_outlined),
-                    selectedIcon: Icon(Icons.analytics, color: Colors.white),
-                    label: 'Analytics',
-                  ),
-                ],
-                labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
