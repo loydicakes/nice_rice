8b9575c97beefdd1d07e21317bb3873b5549df70
janloydpau <cagocojanlloyd@gmail.com>
2025-08-25 15:59:09 +0800

    ewans

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\David\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\David\OneDrive\Documents\nice_rice-main"
+export "FLUTTER_ROOT=C:\Users\Gian\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Gian\AndroidStudioProjects\nice_rice"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
